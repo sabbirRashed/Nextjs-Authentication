1af16bcd29c01b3d52ea05df743ddd009f2193dd
sabbirRashed <srsabbirrahman12@gmail.com>
2026-05-05 20:44:10 +0600

    made first 3 details page at build time using generateStaticParams function

--- src/app/books/[bookId]/page.jsx
+++ src/app/books/[bookId]/page.jsx
@@ -2,7 +2,7 @@ export const generateStaticParams = async()=>{
     const res = await fetch('http://localhost:5000/books');
     const books = await res.json();
 
-    return books.map(book => ({bookId: book.id}))
+    return books.slice(0, 3).map(book => ({bookId: book.id}))
 }
 
 const BookDetails = async({params}) => {
